868f1d270b40f5ffee39880a60a49754d9c21c85
guilherme-gjv <gjv1@aluno.ifal.edu.br>
2026-04-20 18:28:07 -0300

    feat: gateway

---
+++ apps/gateway/src/gateway.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+import { IntrospectAndCompose } from '@apollo/gateway';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            { name: 'users', url: 'http://localhost:3000/graphql' },
+            { name: 'posts', url: 'http://localhost:3001/graphql' },
+          ],
+        }),
+      },
+    }),
+  ],
+})
+export class GatewayModule {}
