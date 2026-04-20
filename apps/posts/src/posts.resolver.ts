aa1e26f3353f91319d885564c55adb3ed8f6e799
guilherme-gjv <gjv1@aluno.ifal.edu.br>
2026-04-20 17:55:20 -0300

    chore: posts resolver

--- apps/posts/src/posts.resolver.ts
+++ apps/posts/src/posts.resolver.ts
@@ -1,23 +1,29 @@
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
-import { PostsService } from './posts.service';
 import { CreatePostInput } from './dto/create-post.input';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { CreatePostCommand } from './commands/create-post.command';
+import { GetPostsQuery } from './queries/get-posts.query';
+import { GetPostQuery } from './queries/get-post.query';
 
 @Resolver('Post')
 export class PostsResolver {
-  constructor(private readonly postsService: PostsService) {}
+  constructor(
+    private readonly commandBus: CommandBus,
+    private readonly queryBus: QueryBus,
+  ) {}
 
   @Mutation('createPost')
   create(@Args('createPostInput') createPostInput: CreatePostInput) {
-    return this.postsService.create(createPostInput);
+    return this.commandBus.execute(new CreatePostCommand(createPostInput));
   }
 
   @Query('posts')
   findAll() {
-    return this.postsService.findAll();
+    return this.queryBus.execute(new GetPostsQuery());
   }
 
   @Query('post')
-  findOne(@Args('id') id: number) {
-    return this.postsService.findOne(id);
+  findOne(@Args('id') id: string) {
+    return this.queryBus.execute(new GetPostQuery(id));
   }
 }
